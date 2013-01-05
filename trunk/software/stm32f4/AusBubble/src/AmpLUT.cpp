c1406f4600f7c9bcb4fd13b4e86e69ece9b735a8
nickdademo@gmail.com <nickdademo@gmail.com@19480e7e-b4a8-3d10-1101-5f042eca9322>
2013-01-05 04:05:43 +0000

    Added lookup table files.

---
+++ trunk/software/stm32f4/AusBubble/src/AmpLUT.cpp
@@ -0,0 +1,22 @@
+#include "AmpLUT.h"
+
+/* WiFi 802.11n Performance Plots in 100% Duty Cycle */
+int wifiN100duty__frequency_MHz[619] = {};
+float wifiN100duty__PDET_V[619] = {};
+float wifiN100duty__gain_dB[619] = {};
+int wifiN100duty__temperature_C[619] = {};
+float wifiN100duty__Pout_dBm[619] = {};
+
+/* WiFi 802.11n Performance Plots in 50% Duty Cycle */
+int wifiN50duty__frequency_MHz[619] = {};
+float wifiN50duty__PDET_V[619] = {};
+float wifiN50duty__gain_dB[619] = {};
+int wifiN50duty__temperature_C[619] = {};
+float wifiN50duty__Pout_dBm[619] = {};
+
+/* WiFi 802.11g Performance Plots in 100% Duty Cycle */
+int wifiG100duty__frequency_MHz[619] = {};
+float wifiG100duty__PDET_V[619] = {};
+float wifiG100duty__gain_dB[619] = {};
+int wifiG100duty__temperature_C[619] = {};
+float wifiG100duty__Pout_dBm[619] = {};
---
+++ trunk/software/stm32f4/AusBubble/src/AmpLUT.h
@@ -0,0 +1,8 @@
+struct ampDataPoint_t
+{
+	int frequency_MHz;
+	float PDET_V;
+	float gain_dB;
+	int temperature_C;
+	float Pout_dBM;
+};
