7c6e692681d30f0ebb8ec64870069efc7068f775
nickdademo@gmail.com <nickdademo@gmail.com@19480e7e-b4a8-3d10-1101-5f042eca9322>
2013-01-05 05:14:54 +0000

    Added Amp LUT data.

--- trunk/software/stm32f4/AusBubble/src/AmpLUT.h
+++ trunk/software/stm32f4/AusBubble/src/AmpLUT.h
@@ -1,8 +1,42 @@
+/************************************************************************/
+/* AusBubble:                                                           */
+/* An open-source RF jammer designed to operate in the 2.4 GHz Wi-Fi    */
+/* frequency block.                                                     */
+/*                                                                      */
+/* AmpLUT.h                                                             */
+/*                                                                      */
+/* Will Robertson <aliask@gmail.com>                                    */
+/* Nick D'Ademo <nickdademo@gmail.com>                                  */
+/*                                                                      */
+/* Copyright (c) 2012 Will Robertson, Nick D'Ademo                      */
+/*                                                                      */
+/* Permission is hereby granted, free of charge, to any person          */
+/* obtaining a copy of this software and associated documentation       */
+/* files (the "Software"), to deal in the Software without restriction, */
+/* including without limitation the rights to use, copy, modify, merge, */
+/* publish, distribute, sublicense, and/or sell copies of the Software, */
+/* and to permit persons to whom the Software is furnished to do so,    */
+/* subject to the following conditions:                                 */
+/*                                                                      */
+/* The above copyright notice and this permission notice shall be       */
+/* included in all copies or substantial portions of the Software.      */
+/*                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                */
+/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS  */
+/* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   */
+/* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN    */
+/* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     */
+/* SOFTWARE.                                                            */
+/*                                                                      */
+/************************************************************************/
+
 struct ampDataPoint_t
 {
 	int frequency_MHz;
 	float PDET_V;
 	float gain_dB;
 	int temperature_C;
-	float Pout_dBM;
+	float Pout_dBm;
 };
