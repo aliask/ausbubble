9b880c410eedff56e29924e4ab4ab2370a030f22
nickdademo@gmail.com <nickdademo@gmail.com@19480e7e-b4a8-3d10-1101-5f042eca9322>
2013-07-12 12:07:43 +0000

    Changed UI header update rate to 250ms.

--- trunk/software/stm32f4/AusBubble/src/Includes.h
+++ trunk/software/stm32f4/AusBubble/src/Includes.h
@@ -66,7 +66,7 @@
 #define FREQ_STEP_HZ        500000
 #define RATE_STEP_HZ        1
 #define DISP_MAX            2
-#define UI_DRAW_COUNT_MAX   500
+#define UI_DRAW_COUNT_MAX   250
 /* Button behaviour */
 #define TICK_RATE_1         200     // Slowest
 #define TICK_RATE_2         100
